ca6e43d834e5c05fe0a6ffa0bf816230df439d3e
Naoki Toyozumi <naoki.romano3@gmail.com>
2024-08-06 10:18:29 +0900

    MSTUserMapperChange

--- src/main/java/jp/co/internous/easylogin/model/mapper/MstUserMapper.java
+++ src/main/java/jp/co/internous/easylogin/model/mapper/MstUserMapper.java
@@ -8,6 +8,6 @@ import jp.co.internous.easylogin.model.domain.MstUser;
 @Mapper
 public interface MstUserMapper {
 
-		@Select("SELECT + FROM mst_user WHERE user_name = #{userName} AND password = #{password}")
+		@Select("SELECT * FROM mst_user WHERE user_name = #{userName} AND password = #{password}")
 		MstUser findByUserNameAndPassword( String userName, String password);
 }
